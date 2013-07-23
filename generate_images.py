94105fb8583057dbf723e2fbed2a472cee674eb2
Logan Williams <loganw@mit.edu>
2013-07-22 23:30:06 -0700

    initial commit

---
+++ generate_images.py
@@ -0,0 +1,55 @@
+import numpy as np
+from scipy.misc import imread, imsave
+import scipy.signal as sig
+
+num_images = 488
+image_width = 1920
+image_height = 1080
+cols_per_image = 10;
+image_loc = "/Users/loganw/Desktop/trees/trees "
+
+# allocate 4-dimensional array for storing all images
+all_images = np.zeros((num_images, image_height, image_width, 3), dtype=np.uint8)
+
+# load all images into memory
+for i in range(0, num_images):
+	print "Loading image " + str(i)
+	all_images[i, :, :, :] = imread("/Users/loganw/Desktop/trees/trees " + str(i+1).zfill(3) + ".jpg")
+
+# calculate some sums for motion statistics. we don't really do anything with these statistics yet, but we might be able to eventually
+row_sums = np.sum(all_images, axis=(2,3))
+column_sums = np.sum(all_images, axis=(1,3))
+
+# create a high pass filter, cutoff 0.1 w_n
+n = 101
+a = sig.firwin(n, cutoff = 0.1, window = "hamming")
+a = -a
+a[n/2] = a[n/2] + 1
+
+filtered_column_sums = np.zeros((image_width+n-1, num_images))
+column_correlations = np.zeros(((image_width-n)*2-1, num_images-1))
+
+filtered_row_sums = np.zeros((image_height+n-1, num_images))
+row_correlations = np.zeros(((image_height-n)*2-1, num_images-1))
+
+for i in range(0, num_images):
+	filtered_column_sums[:,i] = sig.convolve(column_sums[i,:],a)
+	filtered_row_sums[:,i] = sig.convolve(row_sums[i,:],a)
+
+for i in range(0, num_images - 1):
+	column_correlations[:,i] = np.correlate(filtered_column_sums[n-1:-n,i], filtered_column_sums[n-1:-n,i+1], mode='full')
+	row_correlations[:,i] = np.correlate(filtered_row_sums[n-1:-n,i], filtered_row_sums[n-1:-n,i+1], mode='full')
+
+# this gives frame by frame column offsets (linear speed). prone to error.
+column_offsets = np.argmax(column_correlations,axis=0) - (image_width - n - 1)
+
+# this gives frame by frame row offsets (vertical camera shake). prone to error.
+row_offsets = np.argmax(row_correlations,axis=0) - (image_height -n - 1)
+
+new_image = np.zeros((image_height, num_images*cols_per_image, 3), dtype=np.uint8)
+
+for i in range(0, image_width): 
+	print "Processing column " + str(i)
+	for j in range(0, num_images):
+		new_image[:, j*cols_per_image:(j+1)*cols_per_image, :] = all_images[j, :, i:i+cols_per_image, :]
+	imsave("/Users/loganw/Desktop/trees/c_" + str(i) + ".jpg", new_image)
