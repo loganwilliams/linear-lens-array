980ec55b41c1918b26cc1f7e734091a2e6732da7
Logan Williams <loganw@mit.edu>
2013-07-23 09:37:16 -0700

    improved user feedback/output

--- generate_images.py
+++ generate_images.py
@@ -8,13 +8,22 @@ image_height = 1080
 cols_per_image = 10;
 image_loc = "/Users/loganw/Desktop/trees/trees "
 
+digits = math.ceil(math.log(num_images+1,10))
+image_width_digits = math.ceil(math.log(image_width+1,10))
+
+print "Allocating " + str(num_images*image_width*image_height*3/(1024*1024*1024)) " Gb of RAM"
+
 # allocate 4-dimensional array for storing all images
 all_images = np.zeros((num_images, image_height, image_width, 3), dtype=np.uint8)
 
 # load all images into memory
 for i in range(0, num_images):
-	print "Loading image " + str(i)
-	all_images[i, :, :, :] = imread("/Users/loganw/Desktop/trees/trees " + str(i+1).zfill(3) + ".jpg")
+	# progress display
+	for j in range(0,digits+1):
+		print("\b"),
+	print("Loading image " + str(i).zfill(digits)),
+	
+	all_images[i, :, :, :] = imread("/Users/loganw/Desktop/trees/trees " + str(i+1).zfill(digits) + ".jpg")
 
 # calculate some sums for motion statistics. we don't really do anything with these statistics yet, but we might be able to eventually
 row_sums = np.sum(all_images, axis=(2,3))
@@ -48,8 +57,12 @@ row_offsets = np.argmax(row_correlations,axis=0) - (image_height -n - 1)
 
 new_image = np.zeros((image_height, num_images*cols_per_image, 3), dtype=np.uint8)
 
-for i in range(0, image_width): 
-	print "Processing column " + str(i)
+for i in range(0, image_width):
+	# progress display
+	for i in range(0, image_width_digits+1):
+		print("\b"),
+	print("Processing column " + str(i).zfill(image_width_digits)),
+	
 	for j in range(0, num_images):
 		new_image[:, j*cols_per_image:(j+1)*cols_per_image, :] = all_images[j, :, i:i+cols_per_image, :]
 	imsave("/Users/loganw/Desktop/trees/c_" + str(i) + ".jpg", new_image)
