20fcb5e90ba2cbc37735eed58b3a51e4adc004c6
Logan Williams <loganw@mit.edu>
2013-07-25 21:22:32 -0700

    updated perspective code to use load_images

--- generate_images.py
+++ generate_images.py
@@ -13,20 +13,7 @@ image_name = "IMG_1095 "
 output_name = "c_"
 format = ".jpg"
 
-digits = int(math.ceil(math.log(num_images+1,10)))
-image_width_digits = int(math.ceil(math.log(image_width+1,10)))
-
-print "Allocating " + str(num_images*image_width*image_height*3.0/(1024.0*1024*1024)) + " Gb of RAM"
-
-# allocate 4-dimensional array for storing all images
-all_images = np.zeros((num_images, image_height, image_width, 3), dtype=np.uint8)
-
-# load all images into memory
-for i in range(0, num_images):
-	# progress display
-	print "Loading image " + str(i) + "/" + str(num_images)
-	
-	all_images[i, :, :, :] = imread(image_path + image_name + str(i+start_image).zfill(5) + format)
+all_images = load_images_into_memory(image_path, image_name, format, num_images, start_image)
 
 print "Done loading images. Calculating statistics. This may take a while."
 
